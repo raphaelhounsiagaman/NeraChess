57e19652e94936569c6bd5c6aeb23f4f54a4a56a
raphaelhounsiagaman <rhounsiagaman@gmail.com>
2025-11-23 03:51:35 +0100

    Made FirstNNBot class a working NN Bot

--- NeraChessApp/src/ChessPlayers/Bots/FirstNNBot.cpp
+++ NeraChessApp/src/ChessPlayers/Bots/FirstNNBot.cpp
@@ -1,11 +1,38 @@
 #include "FirstNNBot.h"
 
+#include <filesystem>
 #include <algorithm>
 #include <chrono>
+#include <array>
+#include <string>
+#include <stdexcept>
+#include <cctype>
+#include <iostream>
+#include <cmath>
 
-FirstNNBot::FirstNNBot()
+FirstNNBot::FirstNNBot(const std::string& modelPath)
 {
+	if (std::filesystem::exists(modelPath)) 
+		std::cout << "Model found\n";
+	else
+	{
+		std::cout << "Model missing (" + modelPath + ")\n";
+		return;
+	}
 
+	// Initialization of OnnxRuntime
+	std::wstring wide(modelPath.begin(), modelPath.end());
+	const wchar_t* wmodelPath = wide.c_str();
+	m_Session = Ort::Session(m_Env, wmodelPath, m_SessionOptions);
+
+	m_InputVector.resize(1);
+
+}
+
+FirstNNBot::~FirstNNBot()
+{
+	m_Session.release();
+	m_Env.release();
 }
 
 Move FirstNNBot::GetNextMove(const ChessBoard& givenBoard, Timer timer)
@@ -18,17 +45,17 @@ Move FirstNNBot::GetNextMove(const ChessBoard& givenBoard, Timer timer)
 
 	SortMoves(board, legalMoves);
 
-	BoardState boardState = board.GetBoardState();
+	const BoardState& boardState = board.GetBoardState();
 	bool whiteToPlay = boardState.HasFlag(BoardStateFlags::WhiteToMove);
 	int8_t colorMultiplier = whiteToPlay ? 1 : -1;
 
 	Move bestMove{};
-	double bestEval = whiteToPlay ? -999999 : 999999;
+	float bestEval = whiteToPlay ? -999999.f : 999999.f;
 
 	for (const Move& move : legalMoves)
 	{
 		board.MakeMove(move);
-		double eval = Minimax(board, 2, !whiteToPlay, -99999, 99999);
+		float eval = MinimaxSearch(board, 2, !whiteToPlay, -99999, 99999);
 		board.UndoMove(move);
 
 		if ((whiteToPlay && (eval > bestEval)) || (!whiteToPlay && (eval < bestEval)))
@@ -42,19 +69,19 @@ Move FirstNNBot::GetNextMove(const ChessBoard& givenBoard, Timer timer)
 	}
 
 	if (bestMove == 0)
-		return 0;
+		throw;
 
 	return bestMove;
 }
 
-double FirstNNBot::Minimax(ChessBoard& board, int depth, bool whiteMaximizingPlayer, double alpha, double beta)
+float FirstNNBot::MinimaxSearch(ChessBoard& board, int depth, bool whiteMaximizingPlayer, float alpha, float beta)
 {
 	uint16_t gameOverFlags = board.GetGameOver();
 
 	if (gameOverFlags & IS_GAME_OVER)
 	{
 		if (gameOverFlags & IS_CHECKMATE)
-			return whiteMaximizingPlayer ? -99999 : 99999;
+			return whiteMaximizingPlayer ? -99999.f : 99999.f;
 		else
 			return 0;
 	}
@@ -67,12 +94,12 @@ double FirstNNBot::Minimax(ChessBoard& board, int depth, bool whiteMaximizingPla
 
 	if (whiteMaximizingPlayer)
 	{
-		double maxEval = -99999;
+		float maxEval = -99999;
 
 		for (const Move& move : legalMoves)
 		{
 			board.MakeMove(move);
-			double eval = Minimax(board, depth - 1, false, alpha, beta);
+			float eval = MinimaxSearch(board, depth - 1, false, alpha, beta);
 			board.UndoMove(move);
 
 			maxEval = std::max(maxEval, eval);
@@ -85,32 +112,23 @@ double FirstNNBot::Minimax(ChessBoard& board, int depth, bool whiteMaximizingPla
 	}
 	else
 	{
-		double minEval = 99999;
+		float minEval = 99999;
 
 		for (const Move& move : legalMoves)
 		{
 			board.MakeMove(move);
-			double eval = Minimax(board, depth - 1, true, alpha, beta);
+			float eval = MinimaxSearch(board, depth - 1, true, alpha, beta);
 			board.UndoMove(move);
 
 			minEval = std::min(minEval, eval);
 			beta = std::min(beta, eval);
 			if (beta <= alpha)
 				break;
-
 		}
 
 		return minEval;
 	}
 
-
-}
-
-double FirstNNBot::EvaluateBoard(const ChessBoard& board, bool whiteToMove) const
-{
-
-
-	return 0;
 }
 
 void FirstNNBot::SortMoves(const ChessBoard& board, MoveList<218>& moves)
@@ -124,9 +142,9 @@ void FirstNNBot::SortMoves(const ChessBoard& board, MoveList<218>& moves)
 		Piece capturePiece = board.GetPiece(MoveUtil::GetTargetSquare(moves[i]));
 
 		if (capturePiece != PieceType::NO_PIECE)
-			moveScoreGuess += 10 * m_PieceValues[capturePiece] - m_PieceValues[movePiece];
+			moveScoreGuess += 10 * c_PieceValues[capturePiece] - c_PieceValues[movePiece];
 		if (MoveUtil::GetMoveFlags(moves[i]) & MoveFlags::IS_PROMOTION)
-			moveScoreGuess += m_PieceValues[MoveUtil::GetPromoPiece(moves[i])];
+			moveScoreGuess += c_PieceValues[MoveUtil::GetPromoPiece(moves[i])];
 		//if (board.SquareIsAttackedByOpponent(moves[i].TargetSquare))
 		//	moveScoreGuess -= (float)(piece_values[move_piece_type]);
 
@@ -161,4 +179,88 @@ void FirstNNBot::SortMoves(const ChessBoard& board, MoveList<218>& moves)
 	}
 
 	return;
+}
+
+float FirstNNBot::EvaluateBoard(const ChessBoard& board, bool whiteToMove)
+{
+	m_InputArray = BoardToTensor(board);
+
+	Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
+		m_MemoryInfo,
+		m_InputArray.data(),
+		m_InputArray.size(),
+		c_InputShape.data(),
+		c_InputShape.size()
+	);
+	
+	m_InputVector[0] = std::move(inputTensor);
+
+	Ort::RunOptions options;
+
+	std::vector<Ort::Value> outputVector = m_Session.Run(
+		options,
+		&m_InputName,
+		m_InputVector.data(),
+		m_InputVector.size(),
+		&m_OutputName,
+		1
+	);
+	
+	const float* evalPtr = outputVector.front().GetTensorData<float>();
+
+	return *evalPtr;
+}
+
+std::array<float, 19 * 8 * 8> FirstNNBot::BoardToTensor(const ChessBoard& board) const
+{
+	std::array<float, 19 * 8 * 8> out{};
+
+	const BoardState& boardState = board.GetBoardState();
+
+	for (Square square = 0; square < 64; square++)
+	{
+		Piece piece = board.GetPiece(square);
+		if (piece != PieceType::NO_PIECE)
+		{
+			uint8_t file = SquareUtil::GetFile(square);
+			uint8_t rank = SquareUtil::GetRank(square);
+
+			out[piece * 64 + file * 8 + rank] = 1.0f;
+		}
+	}
+
+	if (boardState.HasFlag(BoardStateFlags::WhiteToMove))
+	{
+		float* ptr = &out[12 * 64];
+		std::fill(ptr, ptr + 64, 1.0f);
+	}
+
+	auto fill_castle = [&](int channel) 
+		{
+			float* ptr = &out[channel * 64];
+			std::fill(ptr, ptr + 64, 1.0f);
+		};
+
+	if (boardState.HasFlag(BoardStateFlags::CanWhiteCastleKing)) fill_castle(13);
+	if (boardState.HasFlag(BoardStateFlags::CanWhiteCastleQueen)) fill_castle(14);
+	if (boardState.HasFlag(BoardStateFlags::CanBlackCastleKing)) fill_castle(15);
+	if (boardState.HasFlag(BoardStateFlags::CanBlackCastleQueen)) fill_castle(16);
+
+	// en passant
+	if (boardState.HasFlag(BoardStateFlags::CanEnPassent))
+	{
+		uint8_t file = boardState.enPassantFile;
+		if (file >= 0 && file <= 7)
+		{
+			float* ptr = &out[17 * 64 + file * 8];
+			std::fill(ptr, ptr + 8, 1.0f); // fill only that file
+		}
+	}
+
+	// halfmove clock normalized
+	float val = static_cast<float>(board.GetHalfMoveClock()) / 50.0f;
+	float* ptr = &out[18 * 64];
+	std::fill(ptr, ptr + 64, val);
+
+	return out;
 }
--- NeraChessApp/src/ChessPlayers/Bots/FirstNNBot.h
+++ NeraChessApp/src/ChessPlayers/Bots/FirstNNBot.h
@@ -1,41 +1,63 @@
 #pragma once
+#include <string>
+
+#include "onnxruntime_cxx_api.h"
 
 #include "../ChessPlayer.h"
-#include "Timer.h"
 
 class FirstNNBot : public ChessPlayer
 {
 public:
-
-	FirstNNBot();
+	FirstNNBot(const std::string& modelPath = "model15b.onnx");
+	~FirstNNBot();
 
 	Move GetNextMove(const ChessBoard& givenBoard, Timer timer) override;
 
 private:
 
-	double Minimax(ChessBoard& board, int depth, bool whiteMaximizingPlayer, double alpha, double beta);
+	float MinimaxSearch(ChessBoard& board, int depth, bool whiteMaximizingPlayer, float alpha, float beta);
 
-	double EvaluateBoard(const ChessBoard& board, bool whiteToMove) const;
+	float EvaluateBoard(const ChessBoard& board, bool whiteToMove);
 
-	static void SortMoves(const ChessBoard& board, MoveList& moves);
+	static void SortMoves(const ChessBoard& board, MoveList<218>& moves);
+
+	std::array<float, 19 * 8 * 8> BoardToTensor(const ChessBoard& board) const;
 
 private:
 
-	static constexpr float m_PieceValues[12] = {
-		10.0f, // WHITE_PAWN
-		30.0f, // WHITE_KNIGHT
-		32.0f, // WHITE_BISHOP
-		50.0f, // WHITE_ROOK
-		90.0f, // WHITE_QUEEN
-		10000.0f, // WHITE_KING
-		-10.0f, // BLACK_PAWN
-		-30.0f, // BLACK_KNIGHT
-		-32.0f, // BLACK_BISHOP
-		-50.0f, // BLACK_ROOK
-		-90.0f, // BLACK_QUEEN
-		-10000.0f // BLACK_KING
+	// Const stuff
+	static inline const int c_BoardSize = 8;
+	static inline const int64_t c_NumChannels = 19;
+	static inline const int64_t c_InputTensorSize = 1 * c_NumChannels * c_BoardSize * c_BoardSize;
+
+	static inline const std::array<int64_t, 4> c_InputShape = { 1, c_NumChannels, c_BoardSize, c_BoardSize };
+	static inline const std::array<int64_t, 2> c_OutputShape = { 1, 1 };
+
+	static inline const float c_PieceValues[12] = {
+		1.f,		// white pawn
+		3.f,		// WHITE_KNIGHT
+		3.2f,		// WHITE_BISHOP
+		5.f,		// WHITE_ROOK
+		9.f,		// WHITE_QUEEN
+		1000.f,	// WHITE_KING
+		-1.f,		// BLACK_PAWN
+		-3.f,		// BLACK_KNIGHT
+		-3.2f,		// BLACK_BISHOP
+		-5.f,		// BLACK_ROOK
+		-9.f,		// BLACK_QUEEN
+		-1000.f	// BLACK_KING
 	};
-
 	
+	// AI Stuff
+	Ort::Env m_Env{ ORT_LOGGING_LEVEL_WARNING, "FirstNNBot" };
+	Ort::SessionOptions m_SessionOptions;
+	Ort::Session m_Session{nullptr};
 
+	std::array<float, c_InputTensorSize> m_InputArray = {};
+	Ort::MemoryInfo m_MemoryInfo = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+
+	const char* m_InputName = "input";
+	const char* m_OutputName = "output";
+	
+	std::vector<Ort::Value> m_InputVector;
 };
